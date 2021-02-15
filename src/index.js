65a533edf2bf9ee89db86102d1b21783925bb07b
jina <wlsdk2769@ajou.ac.kr>
2021-02-16 02:26:09 +0900

    Delete todo part 2

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const reducer = (state = [], action) => {
     case ADD_TODO:
       return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter((toDo) => toDo.id !== parseInt(action.id));
     default:
       return state;
   }
@@ -43,7 +43,7 @@ const dispatchAddToDo = (text) => {
 };
 
 const dispatchDeleteToDo = (e) => {
-  const id = e.target.parentNode.id;
+  const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteToDo(id));
 };
 
