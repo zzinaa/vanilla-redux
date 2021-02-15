bbd6a879746796cbba0638f839fe9feff85d83dc
jina <wlsdk2769@ajou.ac.kr>
2021-02-15 23:09:58 +0900

    Store and reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+//data를 modify하는 함수
+//어떤 값을 return 하더라도 state로서 store에 저장됨
+const countModifier = (count = 0) => {
+  console.log(count); // 0
+  return count;
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+//data를 저장하는 공간, countStore에는 4가지의 function 있음
+const countStore = createStore(countModifier);
+console.log(countStore.getState()); // 0
