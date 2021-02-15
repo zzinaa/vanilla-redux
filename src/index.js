a41a0c627ea540401099c0849a709926256e4e39
jina <wlsdk2769@ajou.ac.kr>
2021-02-15 23:41:19 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-//data를 modify하는 함수
-//어떤 값을 return 하더라도 state로서 store에 저장됨
-const countModifier = (count = 0) => {
-  console.log(count); // 0
-  return count;
+//action을 통해 countModifier 에게 메시지를 보낼 수 있음
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-//data를 저장하는 공간, countStore에는 4가지의 function 있음
 const countStore = createStore(countModifier);
-console.log(countStore.getState()); // 0
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
