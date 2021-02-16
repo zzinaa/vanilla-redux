df1409064380c6c8b644dc4c59735ca6dc69b5fb
jina <wlsdk2769@ajou.ac.kr>
2021-02-16 16:12:27 +0900

    How to use DispatchToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,15 @@
 import { React, useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
+    addToDo(text);
     setText("");
   }
   return (
@@ -28,4 +30,12 @@ function mapStateToProps(state) {
   return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
+//mapStateToProps, mapDispatchToProps 둘 중 하나만 이용하고 싶으면
+//connect(null, mapDispatchToProps) 로 작성
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
