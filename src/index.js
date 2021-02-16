814f4139b136e1b64eda55bc07fe174ebf6e8487
jina <wlsdk2769@ajou.ac.kr>
2021-02-16 12:29:47 +0900

    Connect store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./components/App";
+import store from "./store";
 
-ReactDOM.render(<App></App>, document.getElementById("root"));
+ReactDOM.render(
+  //redux store와 연결함
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
