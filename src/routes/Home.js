b8e580edd4225c7aa70ecdbdca45fe0e7be8d1b0
jina <wlsdk2769@ajou.ac.kr>
2021-02-16 15:59:34 +0900

    How to use mapStateToProps function

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import { React, useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
@@ -16,9 +17,15 @@ function Home() {
         <input type="text" value={text} onChange={onChange}></input>
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+//여기서 return 하는 모든 것들은 home의 props 로 전달됨
+//state를 toDos 라는 이름으로 Home component의 props로 전달함
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Home);
