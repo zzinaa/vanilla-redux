c1ce734f3eafca4557d15cc9d03b1c04f82ce878
jina <wlsdk2769@ajou.ac.kr>
2021-02-18 16:27:54 +0900

    Learn about createReducer

--- src/store.js
+++ src/store.js
@@ -1,19 +1,30 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+//위의 것을 아래처럼 바꿀 수 있음(switch, case 없이)
+//아래 처럼 쓰면 state를 수정(mutate) 하기 쉽게 만들어줌
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() }); //mutate
+  }, //return 하지 않음 (방법 1. return 하지 않고 state를 mutate 한다)
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+  //return 함 (방법 2. return 하여 state를 새로 만든다 - 원래 toolkit 쓰기 전처럼)
+});
 
 const store = createStore(reducer);
 
